1f675f2d515462b9e23822c97346b578d2a1f387
Leonardo Graboski Veiga <leonardo.veiga@toradex.com>
2019-11-06 13:37:07 -0300

    patches: cleanup patches and update config.sh

--- config.sh
+++ config.sh
@@ -5,14 +5,15 @@ if [ -f "README.md" ] && [ -d "recipes-aws" ] && [ -d "../../layers" ]
 then
     # patches
     cd ../meta-freescale
-    patch -p1 < ../meta-pasta-demo/patches/meta-freescale/0001-leo-support-wayland-weston-vulkan-etc.patch
-    patch -p1 < ../meta-pasta-demo/patches/meta-freescale/0002-leo-add-gobject-introspection-for-gst-plugins-base.i.patch
+    patch -p1 < ../meta-pasta-demo/patches/meta-freescale/0001-pasta-demo-support-wayland-weston-vulkan-etc.patch
+    patch -p1 < ../meta-pasta-demo/patches/meta-freescale/0002-pasta-demo-add-gobject-introspection-for-gst-plugins.patch
     cd ../meta-toradex-nxp
-    patch -p1 < ../meta-pasta-demo/patches/meta-toradex-nxp/0001-leo-support-wayland-weston.patch
+    patch -p1 < ../meta-pasta-demo/patches/meta-toradex-nxp/0001-pasta-demo-support-wayland-weston.patch
+    patch -p1 < ../meta-pasta-demo/patches/meta-toradex-nxp/0002-pasta-demo-use-older-devicetree-name.patch
     cd ../meta-toradex-torizon
-    patch -p1 < ../meta-pasta-demo/patches/meta-toradex-torizon/0001-leo-changes-to-support-wayland-weston.patch
+    patch -p1 < ../meta-pasta-demo/patches/meta-toradex-torizon/0001-pasta-demo-changes-to-support-wayland-weston.patch
     cd ../openembedded-core
-    patch -p1 < ../meta-pasta-demo/patches/openembedded-core/0001-leo-use-Gstreamer-1.14.4-bindings.patch
+    patch -p1 < ../meta-pasta-demo/patches/openembedded-core/0001-pasta-demo-use-Gstreamer-1.14.4-bindings.patch
     
     # local.conf
     cd ../meta-pasta-demo
